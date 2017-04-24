609f4963ab62c59d14a8b86a64269ca8afaebfa6
Nathan Harris <derjyn@gmail.com>
2017-04-24 10:11:56 -0700

    Add Firesmith to portfolio

---
+++ portfolio/firesmith/js/firesmith.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	document.getElementById("video-bg-vid").playbackRate = 0.5;
+
+	$('nav ul.tabs li').click(function() {
+		var tab_id = $(this).attr('data-tab');
+
+		$('nav ul.tabs li').removeClass('active');
+		$('.tab-content').removeClass('active');
+
+		$(this).addClass('active');
+		$("#" + tab_id).addClass('active');
+	});
+});
